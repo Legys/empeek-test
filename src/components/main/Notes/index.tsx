6e55af9fb964c67f1a1baa97a931aae096a6639c
Alexander Dubrovin <legyssss@gmail.com>
2018-12-12 16:31:22 +0200

    Add basic Notes component.

--- src/components/main/Notes/index.tsx
+++ src/components/main/Notes/index.tsx
@@ -1,8 +1,21 @@
 import * as React from "react";
 
+import { CardBody } from "reactstrap";
+
+import NoteInput from "src/components/main/Notes/NoteInput";
+import NotesList from "src/components/main/Notes/NotesList";
+import CardWrapper from "src/components/ui/CardWrapper";
+
 class Notes extends React.Component {
   public render() {
-    return <div>Notes</div>;
+    return (
+      <CardWrapper title="Items">
+        <CardBody>
+          <NoteInput />
+          <NotesList />
+        </CardBody>
+      </CardWrapper>
+    );
   }
 }
 
