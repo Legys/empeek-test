702ac3a704ec9771c2afa077c648a9a7e858f0ac
Alexander Dubrovin <legyssss@gmail.com>
2018-12-12 16:52:38 +0200

    Adjust input component and button.

--- src/components/main/Notes/NoteInput/index.tsx
+++ src/components/main/Notes/NoteInput/index.tsx
@@ -1,18 +1,21 @@
 import * as React from "react";
 import { Form, FormGroup, Input, Button, Row, Col } from "reactstrap";
+import { STButton, STButtonText } from "./styles";
 
 class NoteInput extends React.Component {
   public render() {
     return (
       <Form>
         <Row>
-          <Col sm={9}>
+          <Col sm={8}>
             <FormGroup>
               <Input type="text" name="email" placeholder="type name here..." />
             </FormGroup>
           </Col>
-          <Col sm={3}>
-            <Button color="primary">Add new</Button>
+          <Col sm={4}>
+            <STButton color="primary">
+              <STButtonText>Add new</STButtonText>
+            </STButton>
           </Col>
         </Row>
       </Form>
---
+++ src/components/main/Notes/NoteInput/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { Button } from "reactstrap";
+
+export const STButton = styled(Button)`
+  &&& {
+    background-color: #27ccc0;
+    border-color: #27ccc0;
+  }
+`;
+
+export const STButtonText = styled.span`
+  padding: 8px 18px;
+`;
