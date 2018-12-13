12f09f6e2bbe8653c40c0dae96d2deed8562526c
Alexander Dubrovin <legyssss@gmail.com>
2018-12-13 17:49:47 +0200

    Add hyphens for word wraps.

--- src/components/main/Comments/CommentsList/CommentItem/styles.ts
+++ src/components/main/Comments/CommentsList/CommentItem/styles.ts
@@ -2,9 +2,15 @@ import styled from "styled-components";
 import ListGroupItem from "reactstrap/lib/ListGroupItem";
 
 export const STGroupItem = styled(ListGroupItem)`
+  overflow-wrap: break-word;
   word-wrap: break-word;
+  -webkit-hyphens: auto;
+  -ms-hyphens: auto;
+  -moz-hyphens: auto;
+  hyphens: auto;
+
+  /* word-wrap: break-word; */
   color: #666666;
-  font-family: "Open Sans";
   font-size: 12px;
   &&& {
     padding: 10px 0px 20px;
